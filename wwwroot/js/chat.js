72f8e443404c33e8f88f71d9697233f0f08b8abb
Alejandro Mejia <almejia@unal.edu.co>
2020-06-12 18:42:33 -0500

    Ajuste visualizacion de la vista Devices Se crea una tabla para desplegar la informacion del dispositivo consultado

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -14,18 +14,24 @@ connection.start().then(function () {
         
 
 connection.on("ReceiveMessage", function (user, message) {
+
+    console.log("Mensaje recibido por el Hub");
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     mensaje = JSON.parse(message);
 
     // Determina los valores para conexion de las otras tramas
     if(mensaje.msg=="mqtt bind ctrl success") {
         tag = mensaje.tag;
+        $("#actual_device_tag").html(tag);
         device_id = mensaje.device_id;
+        $("#actual_device_id").html(device_id);
         device_tkn = mensaje.datas.device_token;
+
+        // Trae todos los parametros de configuracion del dispositivo enlazado
+        getParameters();
+        showActualParameters(mensaje);
     }
-    
-
-
+    console.log('Mensaje del Hub\n');
     console.log(mensaje);
     
     var encodedMsg = user + " says " + msg;
@@ -35,6 +41,11 @@ connection.on("ReceiveMessage", function (user, message) {
     // document.getElementById("mqtt_receive").html(msg);
     console.log('Mensaje recibido por Hub' + encodedMsg);
 });
+
+function showActualParameters(mensaje) {
+    $("#actual_device_tag").html(mensaje.tag);
+    $("#actual_device_time").html(mensaje.dev_cur_pts);
+}
 
 
 
