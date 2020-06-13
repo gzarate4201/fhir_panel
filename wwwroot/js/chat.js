9e403e634cc0816df0807f7d2a09eacfcaa24d33
Santiago <santiuruena@hotmail.com>
2020-06-13 02:36:01 -0500

    Parámetros en la consola de monitoreo y comentarios
    De la trama de parámetros enviada por el device, se extraen y se colocan en la consola html. se colocan comentarios en el archivo MqttClientService.cs

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -45,6 +45,40 @@ connection.on("ReceiveMessage", function (user, message) {
 function showActualParameters(mensaje) {
     $("#actual_device_tag").html(mensaje.tag);
     $("#actual_device_time").html(mensaje.dev_cur_pts);
+    $("#actual_device_name").html(mensaje.datas.basic_parameters.dev_name);
+    $("#actual_device_pass").html(mensaje.datas.basic_parameters.dev_pass);
+    $("#actual_version").html(mensaje.datas.version_info.firmware_ver);
+    $("#actual_firm_date").html(mensaje.datas.version_info.firmware_date);
+    $("#actual_temp_en").html(mensaje.datas.fun_param.temp_dec_en);
+    $("#actual_face_num").html(mensaje.datas.face_recognition_cfg.dec_face_num_cur);
+    $("#actual_stranger_en").html(mensaje.datas.fun_param.stranger_pass_en);
+    $("#actual_face_min").html(mensaje.datas.face_recognition_cfg.dec_face_num_min);
+    $("#actual_make_en").html(mensaje.datas.fun_param.make_check_en);
+    $("#actual_face_max").html(mensaje.datas.face_recognition_cfg.dec_face_num_max);
+    $("#actual_alarm_temp").html(mensaje.datas.fun_param.alarm_temp);
+    $("#actual_dec_interval").html(mensaje.datas.face_recognition_cfg.dec_interval_cur);
+    $("#actual_temp_comp").html(mensaje.datas.fun_param.temp_comp);
+    $("#actual_dec_min").html(mensaje.datas.face_recognition_cfg.dec_interval_min);
+    $("#actual_record_time").html(mensaje.datas.fun_param.record_save_time);
+    $("#actual_dec_max").html(mensaje.datas.face_recognition_cfg.dec_interval_max);
+    $("#actual_save_record").html(mensaje.datas.fun_param.save_record);
+    $("#actual_ip").html(mensaje.datas.network_config.ip_addr);
+    $("#actual_mqtt").html(mensaje.datas.mqtt_protocol_set.enable);
+    $("#actual_mask").html(mensaje.datas.network_config.net_mask);
+    $("#actual_retain").html(mensaje.datas.mqtt_protocol_set.retain);
+    $("#actual_gw").html(mensaje.datas.network_config.gateway);
+    $("#actual_pqos").html(mensaje.datas.mqtt_protocol_set.pqos);
+    $("#actual_ddns1").html(mensaje.datas.network_config.DDNS1);
+    $("#actual_sqos").html(mensaje.datas.mqtt_protocol_set.sqos);
+    $("#actual_ddns2").html(mensaje.datas.network_config.DDNS2);
+    $("#actual_server").html(mensaje.datas.mqtt_protocol_set.server);
+    $("#actual_dhcp").html(mensaje.datas.network_config.DHCP);
+    $("#actual_username").html(mensaje.datas.mqtt_protocol_set.username);
+    $("#actual_mqtt_password").html(mensaje.datas.mqtt_protocol_set.passwd);
+    $("#actual_Topic2Subscribe").html(mensaje.datas.mqtt_protocol_set.topic2subscribe);
+    $("#actual_Topic2Publish").html(mensaje.datas.mqtt_protocol_set.topic2publish);
+    $("#actual_heartbeat").html(mensaje.datas.mqtt_protocol_set.heartbeat);
+
 }
 
 
