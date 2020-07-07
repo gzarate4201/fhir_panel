02491ed1d5de181aab53fc816e047630cd444a79
Santiago <santiuruena@hotmail.com>
2020-07-07 14:35:10 -0500

    Adición de lista de dispositivos en Consulta y Sistema.
    Se agregó la selección de dispositivos a configurar o a hacer consulta  en cada uno de los apartados faltantes: apartado Consulta y apartado Sistema.

--- Controllers/ReporteController.cs
+++ Controllers/ReporteController.cs
@@ -1,23 +1,76 @@
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
 
+// Hub Chat
+using Microsoft.AspNetCore.SignalR;
+using SignalRChat.Hubs;
+
+// using System.Net.Mqtt;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using Mqtt.Client.AspNetCore.Settings;
+
+// Modelos para Base de datos
+using AspStudio.Models;
+using AspStudio.Data;
+
 namespace AspStudio.Controllers
 {
+    
+
     public class ReporteController : Controller
     {
+        // Inyecta la instancia de MQTTnet (mqttClient) que fue creada como
+        // servicio inyectable en StartUp.cs
+        static IMqttClient mqttClient = new MqttFactory().CreateMqttClient();
+
         private readonly ILogger<ReporteController> _logger;
 
-        public ReporteController(ILogger<ReporteController> logger)
+        // Inyeccion clase para manejo de la conexion a BD
+        private readonly ApplicationDbContext dbContext;
+
+        public ReporteController(ILogger<ReporteController> logger, ApplicationDbContext _dbContext)
         {
             _logger = logger;
+            dbContext = _dbContext;
         }
 
         public IActionResult Index()
         {
+            var dispositivos = dbContext.Devices;
+
+
+            List<dynamic> Devices = new List<dynamic>();
+            dynamic device;
+
+            try
+            {
+                foreach (var dispositivo in dispositivos)
+                {
+                    device = new ExpandoObject();
+                    device.id = dispositivo.Id;
+                    device.dev_id = dispositivo.DevId;
+                    device.tag = dispositivo.DevTag;
+                    Devices.Add(device);
+                }
+            }
+            catch (System.Exception e)
+            {
+                System.Console.WriteLine("Error generando lista" + e.Message + e.StackTrace);
+            }
+
+            System.Console.WriteLine(Devices);
+            ViewBag.Devices = Devices;
+            // System.Console.WriteLine(ViewBag.Dispositivos);
             return View();
         }
     }
