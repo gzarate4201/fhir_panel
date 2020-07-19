db03028c8c6aa8439490863791c0174b5aed0208
Alejandro Mejia <amejia@iMac-de-Alejandro.local>
2020-07-18 19:42:56 -0500

    Se corrige el despliegue de la busqueda

--- Controllers/ReporteController.cs
+++ Controllers/ReporteController.cs
@@ -75,6 +75,7 @@ namespace AspStudio.Controllers
                     device.id = dispositivo.Id;
                     device.dev_id = dispositivo.DevId;
                     device.tag = dispositivo.DevTag;
+                    device.token = dispositivo.DevTkn;
                     Devices.Add(device);
                 }
             }
