9661276a39f0b4905a81cf85819785a48ec735be
Alejandro Mejia <alejomejia1@gmail.com>
2020-08-13 13:10:12 -0500

    Se corrige el puerto para la visualizacion de la imagen en los reportes

--- Controllers/ReporteController.cs
+++ Controllers/ReporteController.cs
@@ -823,7 +823,7 @@ namespace AspStudio.Controllers
         public static DateTime UnixTimeStampToDateTime( double unixTimeStamp )
         {
             // Unix timestamp is seconds past epoch
-            System.DateTime dtDateTime = new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc);
+            System.DateTime dtDateTime = new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Local);
             dtDateTime = dtDateTime.AddSeconds( unixTimeStamp ).ToLocalTime();
             return dtDateTime;
         }
--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ FROM mcr.microsoft.com/dotnet/core/aspnet:3.1 AS final
 RUN apt-get update
 RUN apt-get install -y apt-utils
 RUN apt-get install -y libgdiplus
+RUN apt-get install -y nano
 WORKDIR /app
 COPY --from=build /app .
 ENV ALARM_REPORT=False
